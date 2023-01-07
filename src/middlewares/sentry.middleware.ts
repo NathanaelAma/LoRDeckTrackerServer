ccd65fe7036a81cee8f97a338eea83e38469e999
Nathanael Ama <nathanael.ama1@gmail.com>
2023-01-07 11:34:09 +0200

    Add new routes

--- src/middlewares/sentry.middleware.ts
+++ src/middlewares/sentry.middleware.ts
@@ -1,5 +1,5 @@
 import App from '@/app';
-import { NODE_ENV } from '@/config';
+import { NODE_ENV, SENTRY_DSN } from '@/config';
 import * as Sentry from '@sentry/node';
 import * as Tracing from '@sentry/tracing';
 
@@ -7,7 +7,7 @@ const sentryMiddleware = (app: App) => {
   Sentry.init({
     environment: NODE_ENV,
     release: process.env.npm_package_name + '@' + process.env.npm_package_version,
-    dsn: process.env.SENTRY_DSN,
+    dsn: SENTRY_DSN,
     integrations: [new Sentry.Integrations.Http({ tracing: true }), new Tracing.Integrations.Express({ app: app.getServer() })],
     tracesSampleRate: 1.0,
   });
