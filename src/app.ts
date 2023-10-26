db320cc5036f2b2c237fd2abf5246bfabdffbf78
Nathanael Ama <nathanael.ama1@gmail.com>
2023-10-26 10:54:28 +0200

    update test environment

--- src/app.ts
+++ src/app.ts
@@ -60,7 +60,9 @@ class App {
   }
 
   private initializeMiddlewares() {
-    this.app.use(morgan(LOG_FORMAT, { stream }));
+    if (this.env !== 'test') {
+      this.app.use(morgan(LOG_FORMAT, { stream }));
+    }
     this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
     this.app.use(hpp());
     this.app.use(helmet());
