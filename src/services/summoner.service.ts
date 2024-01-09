51d4e54ec7d523f22a3a5255bd0bfa6d4b19df5f
Nathanael Ama <nathanael.ama1@gmail.com>
2024-01-09 20:33:04 +0200

    Add support for RiotRegion in getSummonerByPuuid method

--- src/services/summoner.service.ts
+++ src/services/summoner.service.ts
@@ -5,6 +5,8 @@ import { LeagueRegion } from '@config';
 import galeforce from '@/utils/galeforce';
 import summonerModel from '@/models/summoner.model';
 import { User } from '@/interfaces/users.interface';
+import { RiotRegion } from 'galeforce/dist/riot-api';
+import { logger } from '@/utils/logger';
 
 class SummonerService {
   public summoners = summonerModel;
@@ -17,12 +19,29 @@ class SummonerService {
     return summoner;
   }
 
-  public async getSummonerByPuuid(puuid: string, region: LeagueRegion): Promise<any> {
+  public async getSummonerByPuuid(puuid: string, region: LeagueRegion | RiotRegion): Promise<any> {
     if (isEmpty(puuid)) throw new HttpException(400, 'puuid is empty');
     if (isEmpty(region)) throw new HttpException(400, 'region is empty');
 
-    const summoner: dto.SummonerDTO = await galeforce.lol.summoner().puuid(puuid).region(region).exec();
-    return summoner;
+    function isLeagueRegion(region: LeagueRegion | RiotRegion): region is LeagueRegion {
+      return Object.values(LeagueRegion).includes(region as LeagueRegion);
+    }
+
+    function isRiotRegion(region: LeagueRegion | RiotRegion): region is RiotRegion {
+      return Object.values(RiotRegion).includes(region as RiotRegion);
+    }
+
+    if (isLeagueRegion(region)) {
+      const summoner: dto.SummonerDTO = await galeforce.lol.summoner().puuid(puuid).region(region).exec();
+      logger.info(`Summoner: ${summoner}`);
+      return summoner;
+    } else if (isRiotRegion(region)) {
+      const account: dto.AccountDTO = await galeforce.riot.account.account().puuid(puuid).region(region).exec();
+      logger.info(account);
+      return account;
+    } else {
+      throw new HttpException(400, 'region is not valid');
+    }
   }
 
   public async addSummonerToUser(summonerName: string, region: LeagueRegion, user: User): Promise<any> {
