838582595115d26750dc5cd6ec2f715cbbb8a593
Nathanael Ama <nathanael.ama1@gmail.com>
2024-02-22 12:56:18 +0200

    JS-W1043: fix deepsource issue

--- src/app.ts
+++ src/app.ts
@@ -24,8 +24,8 @@ class App {
 
   constructor(routes: Routes[]) {
     this.app = express();
-    this.env = NODE_ENV || 'development';
-    this.port = PORT || 3000;
+    this.env = NODE_ENV ?? 'development';
+    this.port = PORT ?? 3000;
 
     this.connectToDatabase();
     this.initializeSentry();
