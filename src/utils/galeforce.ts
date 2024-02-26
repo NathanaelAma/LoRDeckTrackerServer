47b48dc677483c870bb91c17772c3702bf62a489
Nathanael Ama <nathanael.ama1@gmail.com>
2024-02-26 13:00:38 +0200

    update galeforce to include dev environment

--- src/utils/galeforce.ts
+++ src/utils/galeforce.ts
@@ -1,10 +1,19 @@
-import { RIOT_API_KEY } from '@/config';
+import { RIOT_API_KEY, NODE_ENV } from '@/config';
 import GaleforceModule from 'galeforce';
 
-const galeforce: GaleforceModule = new GaleforceModule({
+const defaultGaleforce: GaleforceModule = new GaleforceModule({
   'riot-api': {
     key: RIOT_API_KEY,
   },
 });
+
+const debugGalefore: GaleforceModule = new GaleforceModule({
+  'riot-api': {
+    key: RIOT_API_KEY,
+  },
+  debug: ['action'],
+});
+
+const galeforce = NODE_ENV === 'development' ? debugGalefore : defaultGaleforce;
 
 export default galeforce;
